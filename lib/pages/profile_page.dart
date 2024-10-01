2b91dace6bdf4209ef45028c1599c6405a5ea69c
zaydant <zaydantaq@gmail.com>
2024-10-01 16:55:09 +0700

    Profile page now able to edit some details

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:jokiapp/components/my_button.dart';
+import 'package:jokiapp/components/my_textfield.dart';
+import 'package:jokiapp/models/user_model.dart';
 import 'package:jokiapp/models/user_provider.dart';
 import 'package:jokiapp/pages/home_screen.dart';
+import 'package:jokiapp/services/user_services.dart';
 import 'package:provider/provider.dart';
 
-class ProfilePage extends StatelessWidget {
+final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+    GlobalKey<ScaffoldMessengerState>();
+
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool _isLoading = false;
+  final _formKey = GlobalKey<FormState>();
+  UserService _userService = UserService();
+
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getUserDetails();
+  }
+
+  Future<void> getUserDetails() async {
+    final uid = Provider.of<UserProvider>(context, listen: false).uid ?? '';
+    final token = Provider.of<UserProvider>(context, listen: false).token ?? '';
+    UserData? user = await _userService.getUserById(uid, token);
+    setState(() {
+      _phoneController.text = user?.phoneNumber ?? '';
+    });
+  }
+
+  Future<void> updateUser() async {
+    final token = Provider.of<UserProvider>(context, listen: false).token ?? '';
+
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      try {
+        await _userService.updateUser(
+          token,
+          _passwordController.text,
+          _newPasswordController.text,
+          _phoneController.text,
+        );
+
+        scaffoldMessengerKey.currentState?.showSnackBar(
+          const SnackBar(content: Text('User updated successfully')),
+        );
+
+        await getUserDetails();
+      } catch (e) {
+        scaffoldMessengerKey.currentState?.showSnackBar(
+          SnackBar(content: Text('Error updating user: $e')),
+        );
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +83,37 @@ class ProfilePage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         automaticallyImplyLeading: false,
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: Text(
-              'Joki By Dante',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'josefinSans',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
+        leading: const Padding(
+          padding: EdgeInsets.only(left: 20),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.black),
+            onPressed: () async {
+              if (Provider.of<UserProvider>(context, listen: false).token !=
+                      null &&
+                  Provider.of<UserProvider>(context, listen: false)
+                      .token!
+                      .isNotEmpty) {
+                const snackBar = SnackBar(content: Text("Logout Successful"));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                final userProvider =
+                    Provider.of<UserProvider>(context, listen: false);
+                userProvider.logout();
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
+                  ),
+                );
+              } else {
+                const snackBar =
+                    SnackBar(content: Text("No active session found."));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+            },
           ),
         ],
       ),
@@ -63,10 +151,9 @@ class ProfilePage extends StatelessWidget {
                   height: 15,
                 ),
                 CircleAvatar(
-                  radius: 60, // Adjust the size of the circle
+                  radius: 60,
                   backgroundImage: AssetImage('assets/images/profile.png'),
-                  backgroundColor: Colors
-                      .transparent, // Make it transparent if no image is available
+                  backgroundColor: Colors.transparent,
                 ),
                 const SizedBox(
                   height: 15,
@@ -87,47 +174,56 @@ class ProfilePage extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: const Color.fromRGBO(43, 52, 153, 1),
-                  ),
-                  onPressed: () async {
-                    // Check if the token is available
-                    if (Provider.of<UserProvider>(context, listen: false)
-                                .token !=
-                            null &&
-                        Provider.of<UserProvider>(context, listen: false)
-                            .token!
-                            .isNotEmpty) {
-                      // Show a snackbar message
-                      const snackBar =
-                          SnackBar(content: Text("Logout Successful"));
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-                      // Get the UserProvider instance
-                      final userProvider =
-                          Provider.of<UserProvider>(context, listen: false);
-
-                      // Clear the user's data in the provider
-                      userProvider.logout();
-
-                      // navigate to the login screen or show a message
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomeScreen(),
-                        ),
-                      );
-                    } else {
-                      // show a message if the token is not available
-                      const snackBar =
-                          SnackBar(content: Text("No active session found."));
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    }
-                  },
-                  child: const Text('LOG OUT'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text('Phone Number'),
+                  ],
                 ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: _phoneController,
+                  hintText: 'Phone Number',
+                  obscureText: false,
+                  prefixIcon: Icons.phone,
+                  readOnly: false,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text('Old Password'),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: _passwordController,
+                  hintText: 'Old Password',
+                  obscureText: true,
+                  prefixIcon: Icons.lock_outline,
+                  readOnly: false,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text('New Password'),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                MyTextField(
+                  controller: _newPasswordController,
+                  hintText: 'New Password',
+                  obscureText: true,
+                  prefixIcon: Icons.lock,
+                  readOnly: false,
+                ),
+                const SizedBox(height: 10),
+                MyButton(
+                  onTap: updateUser,
+                  buttonText: 'Save',
+                ),
+                const SizedBox(height: 10),
               ],
             ),
           ),
--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -4,42 +4,45 @@ import 'package:http/http.dart' as http;
 import 'package:jokiapp/models/user_model.dart';
 
 class UserService {
-  final String url = "https://api-dante-joki-871423140998.asia-southeast2.run.app/api";
-    
-  Future<List<UserData>> getUsers (String token) async {
-  final uri = Uri.parse('$url/user');
+  final String url =
+      "https://api-dante-joki-871423140998.asia-southeast2.run.app/api";
 
-  try {
-    final response = await http.get(
-      uri,
-      headers: {
-        HttpHeaders.authorizationHeader: 'Bearer $token',
-        'Content-Type': 'application/json',
-      },
-    );
+  Future<List<UserData>> getUsers(String token) async {
+    final uri = Uri.parse('$url/user');
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final List<dynamic> usersJson = data['data'];
+    try {
+      final response = await http.get(
+        uri,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
 
-      return usersJson.map((json) => UserData.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load users');
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final List<dynamic> usersJson = data['data'];
+
+        return usersJson.map((json) => UserData.fromJson(json)).toList();
+      } else {
+        throw Exception('Failed to load users');
+      }
+    } catch (e) {
+      print('Error loading users: $e');
+      throw Exception('Error loading users: $e');
     }
-  } catch (e) {
-    print('Error loading users: $e');
-    throw Exception('Error loading users: $e');
   }
-}
-Future<UserData?> getUserById(String uid, String token) async {
+
+  Future<UserData?> getUserById(String uid, String token) async {
     final uri = Uri.parse('$url/user/$uid');
     final response = await http.get(uri, headers: {
       HttpHeaders.authorizationHeader: 'Bearer $token',
-        'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
     });
 
     if (response.statusCode == 200) {
-      final Map<String, dynamic> responseData = json.decode(response.body)['data'];
+      final Map<String, dynamic> responseData =
+          json.decode(response.body)['data'];
       return UserData.fromJson(responseData);
     } else {
       print('Error fetching transaction: ${response.body}');
@@ -86,4 +89,39 @@ Future<UserData?> getUserById(String uid, String token) async {
       };
     }
   }
+
+  Future<void> updateUser(String token, String? password,
+      String? newPassword, String? phoneNumber) async {
+    final uri = Uri.parse('$url/user');
+
+    try {
+      final response = await http.put(
+        uri,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'newPassword': newPassword ?? '',
+          'oldPassword': password ?? '',
+          'phoneNumber': phoneNumber ?? ''
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        // print('Job taken successfully');
+        final Map<String, dynamic> responseData =
+            json.decode(response.body)['data'];
+        print(responseData);
+      } else {
+        print('Failed to take job: ${response.statusCode}');
+        throw Exception('Failed to take job');
+      }
+    } catch (e) {
+      print(uri);
+      print('Error take job: $e');
+      throw Exception('Error take job: $e');
+    }
+  }
+  
 }
